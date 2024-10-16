84db74f417ce3d6536a7ff1b65cab427691c9b6e
A1exCRE <alsmmm2065@gmail.com>
2024-10-16 13:09:15 +0300

    Update

--- main.cpp
+++ main.cpp
@@ -1,2 +1,9 @@
+#include <iostream>
+void destroy(int ** t, size_t m, size_t n);
+int ** create(size_t m, size_t n);
 int main() {
+   size_t M = 0, N = 0;
+   std::cin >> M >> N;
+   int ** t = create(M, N);
+   destroy(t, M, N);
 }
