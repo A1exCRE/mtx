bb231cc3a0e7553e6a721cf918d0e9b3796476c3
A1exCRE <alsmmm2065@gmail.com>
2024-10-17 23:32:34 +0300

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -1,24 +1,41 @@
 #include <iostream>
 #include "matrix.hpp"
+
 void input(int ** t, size_t m, size_t n) {
-   for (size_t j = 0, j < m, j++) {
-      for (size_t j = 0; j < n; j++) {
-         std::cin >> t[i][j];
-      }
-   }
+  for (size_t i = 0, i < m, i++) {
+    for (size_t j = 0; j < n; j++) {
+      std::cin >> t[i][j];
+    }
+  }
 }
+
 void out(const int * const * t, size_t m, size_t n) {
-   for (size_t i = 0; i < m; ++i) {
-      std::cout t[i][j];
-      for (size_t j = 0; j < n; j++) {
-         std::cout << " " << t[i][j];
-      }
-      std::cout << "\n";
-   }
+  for (size_t i = 0; i < m; ++i) {
+    std::cout t[i][j];
+    for (size_t j = 0; j < n; ++j) {
+      std::cout << " " << t[i][j];
+    }
+    std::cout << "\n";
+  }
 }
+
 void destroy(int** t, size_t m) {
-   for (size_t i = 0; i < m; ++i) {
-      delete[] t[i];
-   }
-   delete []t;
+  for (size_t i = 0; i < m; ++i) {
+    delete[] t[i];
+  }
+  delete []t;
+}
+
+int ** create(size_t m, size_t n) {
+  int ** t = new int*[m];
+  size_t created = 0;
+  try {
+    for (;created < m; ++created) {
+      t[created] = new int[n];
+    }
+  } catch (const std::bad_alloc & e) {
+    destroy(t, created);
+    throw;
+  }
+  return t;
 }
