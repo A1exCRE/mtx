eb660fad0904206cf40c575ac25205326f1f34ae
A1exCRE <alsmmm2065@gmail.com>
2024-10-17 23:47:36 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
-#include <matrix.cpp>
+#include "matrix.hpp"
+
 int main() {
-   size_t M = 0, N = 0;
-   std::cin >> M >> N;
-   int ** t = create(M, N);
-   destroy(t, M, N);
+  size_t M = 0, N = 0;
+  std::cin >> M >> N;
+  if (!std::cin) {
+    return 1;
+  }
+  int ** t = nullptr;
+  try {
+    t = create(M, N);
+  } catch (const std::bad_alloc & e) {
+    std::cerr << "Out of memory\n";
+    return 1;
+  }
+  input(t, M, N);
+  out(t, M, N);
+  destroy(t, M);
 }
